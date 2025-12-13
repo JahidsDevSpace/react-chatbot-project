e25831a94b4fada01d13e45fea5618b7e8aa3e7d
Jahid Hasan <jahidhasan08m@gmail.com>
2025-12-13 22:12:56 +0600

    Generate tracking page HTML using backend

--- ecommerce-project/src/pages/NotFoundPage.jsx
+++ ecommerce-project/src/pages/NotFoundPage.jsx
@@ -1,12 +1,12 @@
 import { Header } from '../components/Header';
 import './NotFoundPage.css';
 
-export function NotFoundPage() {
+export function NotFoundPage({cart}) {
   return (
     <>
       <title>404 Not Fount</title>
 
-      <Header />
+      <Header cart={cart} />
 
       <div className='not-found-page'>
         <p>Page not found.</p>
