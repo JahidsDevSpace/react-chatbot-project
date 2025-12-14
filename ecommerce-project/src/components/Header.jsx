8be7df4b485d56a78255986009f942c2e6f28633
Jahid Hasan <jahidhasan08m@gmail.com>
2025-12-15 01:13:37 +0600

    Create search products feature

--- ecommerce-project/src/components/Header.jsx
+++ ecommerce-project/src/components/Header.jsx
@@ -1,4 +1,5 @@
-import { NavLink } from 'react-router';
+import { useState } from "react";
+import { NavLink, useNavigate, useSearchParams } from 'react-router';
 import LogoWhite from '../assets/images/logo-white.png';
 import MobileLogoWhite from '../assets/images/mobile-logo-white.png';
 import CartIcon from '../assets/images/icons/cart-icon.png';
@@ -6,6 +7,29 @@ import SearchIcon from '../assets/images/icons/search-icon.png';
 import './Header.css';
 
 export function Header({ cart }) {
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+  const searchText = searchParams.get('search');
+  const [search, setSearch] = useState(searchText || ''); 
+
+  const updateSearchInput = (event) => {
+    setSearch(event.target.value);
+
+    if (event.target.value === "") {
+      navigate("/");
+    }
+  }
+  
+  const searchProduct = () => {
+    navigate(`/?search=${search}`);
+  }
+
+  const handleSearchKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      searchProduct();
+    }
+  }
+
   let totalQuantity = 0;
 
   if (cart) {
@@ -24,9 +48,9 @@ export function Header({ cart }) {
       </div>
 
       <div className="middle-section">
-        <input className="search-bar" type="text" placeholder="Search" />
+        <input className="search-bar" type="text" placeholder="Search" value={search} onChange={updateSearchInput} onKeyDown={handleSearchKeyDown} />
 
-        <button className="search-button">
+        <button className="search-button" onClick={searchProduct} >
           <img className="search-icon" src={SearchIcon} />
         </button>
       </div>
