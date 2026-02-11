ee772a77f9192b963a91e40351193c687ea0cd70
Jahid Hasan <jahidhasan08m@gmail.com>
2026-02-11 23:21:34 +0600

    Create HomePage

--- demo-portfolio/src/App.tsx
+++ demo-portfolio/src/App.tsx
@@ -5,8 +5,7 @@ import Projects from "./pages/Projects";
 import Blog from "./pages/Blog";
 import BlogPost from "./pages/BlogPost";
 import About from "./pages/About";
-import Heading from "./components/Heading";
-import SubHeading from "./components/SubHeading";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
@@ -14,23 +13,8 @@ function App() {
     <main className="flex min-h-screen items-start justify-start">
       <Container className="min-h-[200vh] p-4 pt-18 md:pt-20 md:pb-10">
         <Navbar />
-
         <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <Heading>Jahid Hasan</Heading>
-                <SubHeading>
-                  I'm a software engineer with a passion for building scalable and efficient systems. I'm currently working as a software
-                  engineer at Google.
-                </SubHeading>
-                <Projects />
-                <Blog />
-              </>
-            }
-          />
-          {/* <Projects /> */}
+          <Route index element={<HomePage />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/blog" element={<Blog />} />
           <Route path="/blog/:slug" element={<BlogPost />} />
--- demo-portfolio/src/pages/HomePage.tsx
+++ demo-portfolio/src/pages/HomePage.tsx
@@ -1,7 +1,21 @@
+import Heading from "../components/Heading";
+import SubHeading from "../components/SubHeading";
+import Projects from "./Projects";
+import Blog from "./Blog";
+
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <>
+      <Heading>Jahid Hasan</Heading>
+      <SubHeading>
+        I'm a software engineer with a passion for building scalable and
+        efficient systems. I'm currently working as a software engineer at
+        Google.
+      </SubHeading>
+      <Projects />
+      <Blog />
+    </>
+  );
+};
 
-export default HomePage
+export default HomePage;
