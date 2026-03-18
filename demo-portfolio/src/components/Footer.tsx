a59571523ec391a5e2019e176460a974d794459e
Jahid Hasan <jahidhasan08m@gmail.com>
2026-03-18 21:06:42 +0600

    Generate Footer html

--- demo-portfolio/src/components/Footer.tsx
+++ demo-portfolio/src/components/Footer.tsx
@@ -5,31 +5,39 @@ import IconBrandLinkedIn from '/brand-linkedin.svg';
 import Container from "./Container";
 
 const Footer = () => {
+  const footerItems = [
+    {
+      href: '#',
+      icon: IconBrandX,
+      alt: 'X'
+    },
+    {
+      href: 'https://github.com/JahidsDevSpace',
+      icon: IconBrandGithub,
+      alt: 'Github'
+    },
+    {
+      href: 'https://www.linkedin.com/in/mdjahidhasan24/',
+      icon: IconBrandLinkedIn,
+      alt: 'LinkedIn'
+    }
+  ]
+
   return (
     <Container className="flex justify-between border-t border-neutral-100 py-3 px-10">
       <p className="text-xs text-neutral-500">Built with Jahid Hasan</p>
       <div className="flex items-center justify-center gap-4">
-        <Link to="#" target="blank">
-          <img
-            src={IconBrandX}
-            alt="X"
-            className="size-4 text-neutral-500 hover:text-neutral-700"
-          />
-        </Link>
-        <Link to="https://github.com/JahidsDevSpace" target="blank">
-          <img
-            src={IconBrandGithub}
-            alt="Github"
-            className="size-4 text-neutral-500 hover:text-neutral-700"
-          />
-        </Link>
-        <Link to="https://www.linkedin.com/in/mdjahidhasan24/" target='_blank'>
-          <img
-            src={IconBrandLinkedIn}
-            alt="LinkedIn"
-            className="size-4 text-neutral-500 hover:text-neutral-700"
-          />
-        </Link>
+        {footerItems.map((item, index) => {
+          return (
+            <Link to={item.href} key={index} target="blank">
+              <img
+                src={item.icon}
+                alt={item.alt}
+                className="size-4 text-neutral-500 hover:text-neutral-700"
+              />
+            </Link>
+          );
+        })}
       </div>
     </Container>
   );
